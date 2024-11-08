1b7bf1ec0175db21d8c84ac6bd30b95a08122665
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2024-11-09 12:03:49 +1300

    Initial Commit

---
+++ Scheduler App/Scheduler_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scheduler_AppApp.swift
+//  Scheduler App
+//
+//  Created by Nikita Shvad on 09.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Scheduler_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
